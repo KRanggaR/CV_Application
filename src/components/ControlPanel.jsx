2ff133e2687c6127a85853c9a20bce2a99872843
KRanggaR <krangar83540@gmail.com>
2025-04-17 15:06:18 +0530

    Added Example, Clear Buttons functionality

--- src/components/ControlPanel.jsx
+++ src/components/ControlPanel.jsx
@@ -1,18 +1,78 @@
-import React, {useState} from 'react'
+import React from 'react'
 import '../styles/defaultView.css';
 import Button from '../UI/Button.jsx';
 import { FileUser, FilePlus, Trash2 } from 'lucide-react';
 
 
-function ControlPanel() {
-    const [isCleared, setIsCleared] = useState(false);
+function ControlPanel({ setFormData}) {
+    // const [isCleared, setIsCleared] = useState(false);
 
     const handleExample = () => {
-        window.location.href = "https://www.google.com";
+        setFormData((prev) => ({
+            ...prev,
+            personal: {
+              fullname: 'Kishant Rangar',
+              email: 'krangar@notmock.com',
+              phone: '7300529637',
+              address: 'IT Park, Dehradun',
+              summary: 'A dedicated B.Tech student majoring in Computer Science',
+            },
+            
+            education: [
+                {
+                    degree: 'B.Tech in Computer Science', 
+                    school: 'UPES, Dehradun', 
+                    startDate: '2020', 
+                    endDate: '2024', 
+                    location: 'Dehradun, Uttarakhand'
+                },
+                {
+                    degree: 'Intermediate (XII)', 
+                    school: 'RRSS', 
+                    startDate: '2019', 
+                    endDate: '2020', 
+                    location: 'Uttarkashi, Uttarakhand'
+                },
+                
+            ],
+            experience: [
+                {
+                    jobTitle : "Programmer Intern",
+                    company: "EVON Technologies", 
+                    startDate : "2024",
+                    endDate : "Current",
+                    location : "A-5 IT Park, Sahastradhara Road, Dehradun",
+                    description : "Working on React, a Javascript Framework.",
+                },
+                {
+                    jobTitle: 'Intern', 
+                    company: 'PwC', 
+                    startDate: '2023', 
+                    endDate: '2023', 
+                    location: 'Gurugram/Hybrid',
+                    description : "Enhanced database performance by optimizing SQL queries and indexing strategies.",
+                },
+
+            ],
+          }));
     };
+
+    // index
     const handleClear = () => {
-        setIsCleared(true);
-    };
+        setFormData((prev) => ({
+          ...prev,
+          personal: {
+            fullname: '',
+            email: '',
+            phone: '',
+            address: '',
+            summary: '',
+          },
+          education: [],
+          experience: [],
+        }));
+      };
+
     const handleDownload = () => {
         window.location.href = "https://www.google.com";
     };
