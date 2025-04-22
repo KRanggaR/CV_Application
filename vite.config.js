a1ecbd3d9bbe73856e7f4e1dbb0cfb3017cd518d
KRanggaR <krangar83540@gmail.com>
2025-04-23 00:42:02 +0530

    updated vite.config

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/CV_Application/',
   plugins: [react()],
+  base: '/CV_Application',
   optimizeDeps: {
     include: ['html2canvas'],
   },
